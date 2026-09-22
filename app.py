e5c0c6012bd8c94cdab541b984e36e266cfbe7ef
axmadjanrrr5555-blip <axmadjanrrr5555@gmail.com>
2026-09-22 21:09:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -274,3 +274,38 @@ else:
                     }
                 )
                 st.success(f"Нәтиже: {score} / {len(q_list)}")
+
+body {
+    margin: 0;
+    background-image: url("background.jpg");
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+    font-family: Arial, sans-serif;
+    color: white;
+}
+
+.hero {
+    position: relative;
+    min-height: 100vh;
+    overflow: hidden;
+}
+
+.name {
+    position: absolute;
+    margin: 0;
+    font-size: 150px;
+    font-weight: 400;
+    color: #eeeeee;
+    letter-spacing: -5px;
+}
+
+.name1 {
+    top: 20%;
+    left: 13%;
+}
+
+.name2 {
+    top: 43%;
+    right: 10%;
+}
